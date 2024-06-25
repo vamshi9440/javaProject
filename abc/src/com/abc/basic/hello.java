e5718b4d6b22273b38f22d71f3a3866e01ca414e
muvam <muvam@10.0.0.227>
2024-06-25 17:05:32 -0400

    new changes have been made

--- abc/src/com/abc/basic/hello.java
+++ abc/src/com/abc/basic/hello.java
@@ -2,7 +2,7 @@ package com.abc.basic;
 
 public class hello {
 	static int age=22;
-	static String college="Pace Universityy";
+	static String college="Pace Universityy newyork";
 	public static void main(String args[]) {
 		System.out.print("Hello i am vamsi krishna "+ "my age is : "+ age +" ");
 		System.out.println("My University is :"+college);
