59d93c18be14982db76c84551ba676312feda8c4
muvam <muvam@10.0.0.227>
2024-06-25 16:40:49 -0400

    jjjj

--- abc/src/com/abc/basic/hello.java
+++ abc/src/com/abc/basic/hello.java
@@ -2,7 +2,7 @@ package com.abc.basic;
 
 public class hello {
 	static int age=22;
-	static String college="Pace University";
+	static String college="Pace Universityy";
 	public static void main(String args[]) {
 		System.out.print("Hello i am vamsi krishna "+ "my age is : "+ age +" ");
 		System.out.println("My University is :"+college);
---
+++ abc/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author muvam
+ *
+ */
+module abc {
+}
